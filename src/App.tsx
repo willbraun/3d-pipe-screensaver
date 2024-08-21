a044074ac50b2e06cd799538c42f32807cefec7d
willbraun <williamhbraun1@gmail.com>
2024-08-20 22:39:23 -0400

    updated directional light

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export const App = () => {
 			<OrbitControls />
 			<PerspectiveCamera makeDefault fov={100} position={[0, 0, 75]} />
 			<ambientLight intensity={0.1} />
-			<directionalLight position={[1, 1, 1]} />
+			<directionalLight position={[0.5, 1, 1]} />
 
 			<Pipe start={new Vector3(-12, 0, 0)} end={new Vector3(-12, 4, 0)} pointsRef={pointsRef} color={'orange'} />
 			<Pipe start={new Vector3(12, 0, 0)} end={new Vector3(12, -8, 0)} pointsRef={pointsRef} color={'limegreen'} />
